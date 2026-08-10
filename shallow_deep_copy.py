68d13a74ccaee93cdf9a22f5c5ddbd5dcaa5d3f6
Ravinder Singh <r.singh.negi85@gmail.com>
2026-08-10 13:42:08 +0530

    Updated

---
+++ shallow_deep_copy.py
@@ -0,0 +1,57 @@
+
+# Simple shallow copy: Now numbers and new_numbers are two separate lists:
+
+numbers = [1, 2, 3]
+
+new_numbers = numbers.copy()
+
+print(new_numbers)
+# [1, 2, 3]
+
+new_numbers.append(4)
+
+print(numbers)
+# [1, 2, 3]
+
+print(new_numbers)
+# [1, 2, 3, 4]
+
+# both variables refer to the same list:
+# numbers ─────┐
+#             ↓
+#          [1, 2, 3]
+#             ↑
+# new_numbers ─┘
+
+#The easiest way to understand shallow copy vs deep copy is with a nested list.
+
+#1. Shallow Copy : A shallow copy creates a new outer object, but the nested objects are still shared.
+
+import copy
+
+original = [[1, 2], [3, 4]]
+
+shallow = copy.copy(original)
+
+shallow[0].append(5)
+
+print(original)
+# [[1, 2, 5], [3, 4]]
+
+print(shallow)
+# [[1, 2, 5], [3, 4]]
+
+
+# 2. Deep Copy:  A deep copy creates a new outer object and new nested objects.
+
+original = [[1, 2], [3, 4]]
+
+deep = copy.deepcopy(original)
+
+deep[0].append(5)
+
+print(original)
+# [[1, 2], [3, 4]]
+
+print(deep)
+# [[1, 2, 5], [3, 4]]
--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 # tuple : A tuple is immutable, which means its values cannot be changed after it is created.
-
 tuple_var = ("Ravi",40,"Cloud Eng")
-print(type(tuple_var))
+print('===' + str(type(tuple_var)) + '===')
 
 # Count tuple:
 # It takes a value, searches through the entire list of tuples, and returns the count of matching values.
@@ -13,3 +12,26 @@ print(tuple_var.index("Ravi"))
 
 # Access Tuple by Index
 print(tuple_var[1])
+
+
+# List in python:
+list_var = ["ravi",39,"software eng."]
+print('===' + str(type(list_var)) + '===')
+list_var.append('Cloud+AI Eng.')
+print(list_var[0] +' is moved from '+ list_var[2] + ' => '+ list_var[3])
+
+# reverse List:
+list_var.reverse()
+print(list_var)
+
+#copy method: 
+numbers = [1, 2, 3]
+
+new_numbers = numbers.copy() # Now numbers and new_numbers are two separate lists:
+
+print(new_numbers)
+# [1, 2, 3]
+
+
+
+
